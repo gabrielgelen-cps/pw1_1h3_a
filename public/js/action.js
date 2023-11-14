e0645869df81d9de90733b259243eb14bad98e95
gabrielgelen-cps <gabriel.gelen@etec.sp.gov.br>
2023-11-14 20:51:01 -0300

    Aula 1411 Fundamentos de DOM HTML

--- public/js/action.js
+++ public/js/action.js
@@ -1,14 +1,25 @@
-function somaNumero(n1, n2, op){
-    let resultado = 0;
-    if(op==="c"){
+function calculaNumeros(){
+    let n1 = parseInt (document.getElementById("usuario").value)
+    let n2 = parseInt (document.getElementById("senha").value)
+    let op = document.getElementById("operation").value
+    let resultado
+    if (op === "+") {
         resultado = (n1 + n2)
-        console.table(resultado)
-        return
-    }
-
-    if(op==="a"){
+        alert("Soma: " + n1 + " " + op + " " + n2 + " = " + resultado)
+    }else if (op === "-") {
         resultado = (n1 - n2)
-        alert(resultado)
-        return
+        alert("Subtração: " + n1 + " " + op + " " + n2 + " = " + resultado)
+    }else if (op === "*") {
+        resultado = (n1 * n2)
+        alert("Multiplicação: " + n1 + " " + op + " " + n2 + " = " + resultado)
+    }else if (op === "/") {
+        resultado = (n1 / n2)
+        alert("Divisão: " + n1 + " " + op + " " + n2 + " = " + resultado)
+    }else if (op === "sqrt") {
+        resultado = Math.sqrt(n1 + n2)
+        alert("Raiz: " + resultado)
+    }else if (op === "pw") {
+        resultado = Math.pow(n1, n2)
+        alert("Potência: " + resultado)
     }
 }
